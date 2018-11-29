dcb3b0bbeed878a688a0498894c4fd9693a1244c
Rick Fleming <elfeming111@yahoo.com>
2018-11-28 21:01:21 -0700

    cont. Experimenting with delete

--- index.js
+++ index.js
@@ -68,7 +68,14 @@ server.delete(`/api/users/:id`, (req, res)=>{
             res.status(404).json({message: 'The user with the specified ID does not exist.'});
         }
         else{
-            res.status(204);
+            // TODO: See if this needs revised
+            db.find()
+            .then(users=>{
+                res.status(200).json(users);
+            })
+            .catch(error=>{
+                res.status(500).json({error: 'The users information could not be retrieved.'});
+            });
         }
     })
     .catch(error=>{
